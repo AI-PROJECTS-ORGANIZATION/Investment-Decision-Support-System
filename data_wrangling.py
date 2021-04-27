b8e64501d1b18e0a5290243a2ff8d01bc99d5930
Dusan Stevic <stevicdule@gmail.com>
2021-04-27 13:06:03 +0200

    data wrangling refactoring

--- data_wrangling.py
+++ data_wrangling.py
@@ -53,7 +53,8 @@ def data_wrangling_corpus1(path):
     df.drop_duplicates(inplace=True)
     print("Dataframe length after the complete duplicates drop:",len(df))
     # Remove Duplicate Rows in place (remove partial duplicates = same text different sentiment)
-    remove_partial_duplicates(df)
+    #remove_partial_duplicates(df)
+    df.drop_duplicates(subset=['text'], keep=False, inplace=True)
     print("Dataframe length after the partial duplicates drop:",len(df))
     # Set unique sentiment labels (-1: negative, 0: neutral, 1: positive). 
     # Sentiment labels mapping:
@@ -87,7 +88,8 @@ def data_wrangling_corpus1(path):
     print('The number of negative tweets in dataset:',negative)
     print('The number of neutral tweets in dataset:',neutral) 
     print('The number of positive tweets in dataset:',positive)
-    df.to_csv('apple1.csv')
+    df.to_csv('data\corpora\corpus1.csv')
+    df.to_pickle('data\corpora\corpus1.pkl')
 
 def data_wrangling_corpus2(path):
     df = pd.read_csv(path)
@@ -104,7 +106,8 @@ def data_wrangling_corpus2(path):
     df.drop_duplicates(inplace=True)
     print("Dataframe length after the complete duplicates drop:",len(df))
     # Remove Duplicate Rows in place (remove partial duplicates = same text different sentiment)
-    remove_partial_duplicates(df)
+    #remove_partial_duplicates(df)
+    df.drop_duplicates(subset=['text'], keep=False, inplace=True)
     print("Dataframe length after the partial duplicates drop:",len(df))
     # Set unique sentiment labels (-1: negative, 0: neutral, 1: positive). 
     # Sentiment labels mapping:
@@ -138,7 +141,8 @@ def data_wrangling_corpus2(path):
     print('The number of negative tweets in dataset:',negative)
     print('The number of neutral tweets in dataset:',neutral) 
     print('The number of positive tweets in dataset:',positive)
-    df.to_csv('apple2.csv')
+    df.to_csv('data\corpora\corpus2.csv')
+    df.to_pickle('data\corpora\corpus2.pkl')
 
 def data_wrangling_corpus3(path):
     # Read csv file to Dataframe with ; delimiter 
@@ -154,7 +158,8 @@ def data_wrangling_corpus3(path):
     df.drop_duplicates(inplace=True)
     print("Dataframe length after the complete duplicates drop:",len(df))
     # Remove Duplicate Rows in place (remove partial duplicates = same text different sentiment)
-    remove_partial_duplicates(df)
+    #remove_partial_duplicates(df)
+    df.drop_duplicates(subset=['text'], keep=False, inplace=True)
     print("Dataframe length after the partial duplicates drop:",len(df))
     # Set unique sentiment labels (-1: negative, 0: neutral, 1: positive). 
     # Sentiment labels mapping:
@@ -188,7 +193,8 @@ def data_wrangling_corpus3(path):
     print('The number of negative tweets in dataset:',negative)
     print('The number of neutral tweets in dataset:',neutral) 
     print('The number of positive tweets in dataset:',positive)
-    df.to_csv('apple3.csv')
+    df.to_csv('data\corpora\corpus3.csv')
+    df.to_pickle('data\corpora\corpus3.pkl')
 
 def data_wrangling_corpus4(path):
     df = pd.read_csv(path)
@@ -203,7 +209,8 @@ def data_wrangling_corpus4(path):
     df.drop_duplicates(inplace=True)
     print("Dataframe length after the complete duplicates drop:",len(df))
     # Remove Duplicate Rows in place (remove partial duplicates = same text different sentiment)
-    remove_partial_duplicates(df)
+    #remove_partial_duplicates(df)
+    df.drop_duplicates(subset=['text'], keep=False, inplace=True)
     print("Dataframe length after the partial duplicates drop:",len(df))
     # Set unique sentiment labels (-1: negative, 0: neutral, 1: positive). 
     # Sentiment labels mapping:
@@ -237,7 +244,8 @@ def data_wrangling_corpus4(path):
     print('The number of negative tweets in dataset:',negative)
     print('The number of neutral tweets in dataset:',neutral) 
     print('The number of positive tweets in dataset:',positive)
-    df.to_csv('apple4.csv')
+    df.to_csv('data\corpora\corpus4.csv')
+    df.to_pickle('data\corpora\corpus4.pkl')
 
 # Inplace transformation reduces memory consumption. 
 # The same DataFrame is used for transformation. 
@@ -301,7 +309,8 @@ def data_wrangling_corpus5(path):
     df.drop_duplicates(inplace=True)
     print("Dataframe length after the complete duplicates drop:",len(df))
     # Remove Duplicate Rows in place (remove partial duplicates = same text different sentiment)
-    remove_partial_duplicates(df)
+    #remove_partial_duplicates(df)
+    df.drop_duplicates(subset=['text'], keep=False, inplace=True)
     print("Dataframe length after the partial duplicates drop:",len(df))
     # Set unique sentiment labels (-1: negative, 0: neutral, 1: positive). 
     # Sentiment labels mapping:
@@ -331,7 +340,8 @@ def data_wrangling_corpus5(path):
     print('The number of negative tweets in dataset:',negative)
     print('The number of neutral tweets in dataset:',neutral) 
     print('The number of positive tweets in dataset:',positive)
-    df.to_csv('apple5.csv')
+    df.to_csv('data\corpora\corpus5.csv')
+    df.to_pickle('data\corpora\corpus5.pkl')
 
 def data_wrangling_corpus6(path):
     # Read Big Data files in chunk size
@@ -383,20 +393,21 @@ def data_wrangling_corpus6(path):
     print('The number of negative tweets in dataset:',negative)
     print('The number of neutral tweets in dataset:',neutral) 
     print('The number of positive tweets in dataset:',positive)
-    df.to_csv('apple6.csv')
+    df.to_csv('data\corpora\corpus6.csv')
+    df.to_pickle('data\corpora\corpus6.pkl')
 
 if __name__ == '__main__':
-    #path1 = 'Apple-Twitter-Sentiment-DFE.csv'
-    #data_wrangling_corpus1(path1)
-    #path2 = 'all-data.csv'
-    #data_wrangling_corpus2(path2)
-    #path3 = 'tweets_labelled_09042020_16072020.csv'
-    #data_wrangling_corpus3(path3)
-    #path4 = 'twt_sample.csv'
-    #data_wrangling_corpus4(path4)
-    #path5 = 'Combined_News_DJIA.csv'
-    #data_wrangling_corpus5(path5)
-    path6 = 'news_train_from2013.csv'
+    path1 = r'data\raw_data\labeled_tweets\Apple-Twitter-Sentiment-DFE.csv'
+    data_wrangling_corpus1(path1)
+    path2 = r'data\raw_data\labeled_tweets\all-data.csv'
+    data_wrangling_corpus2(path2)
+    path3 = r'data\raw_data\labeled_tweets\tweets_labelled_09042020_16072020.csv'
+    data_wrangling_corpus3(path3)
+    path4 = r'data\raw_data\labeled_tweets\twt_sample.csv'
+    data_wrangling_corpus4(path4)
+    path5 = r'data\raw_data\labeled_tweets\Combined_News_DJIA.csv'
+    data_wrangling_corpus5(path5)
+    path6 = r'data\raw_data\labeled_tweets\news_train_from2013.csv'
     data_wrangling_corpus6(path6)
 
     
